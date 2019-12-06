0d56db5636af0a379e4921246734b562474407e3
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2019-12-06 17:25:25 +0100

    Saving values already calculated inside an HashMap for later use

--- src/lib.rs
+++ src/lib.rs
@@ -5,34 +5,32 @@
  * 
  * */
 
-use std::marker::PhantomData;
+use std::collections::HashMap;
+use std::hash::Hash;
 
-pub struct Cacher<C, V, R>
-    where C: Fn(&[V]) -> R, R: Copy
+pub struct Cacher<'a, C, V, R>
+    where C: Fn(&[V]) -> R, R: Clone, V: Eq + Hash
 {
-    value: Option<R>,
+    value: HashMap<&'a [V], R>,
     calc: C,
-    phantom: PhantomData<V>,
 }
 
-impl<C, V, R> Cacher<C, V, R>
-    where C: Fn(&[V]) -> R, R: Copy
+impl<'a, C, V, R> Cacher<'a, C, V, R>
+    where C: Fn(&[V]) -> R, R: Clone, V: Eq + Hash
 {
-    pub fn new(calc: C) -> Cacher<C, V, R> {
+    pub fn new(calc: C) -> Cacher<'a, C, V, R> {
         Cacher {
-            value: None,
+            value: HashMap::new(),
             calc,
-            phantom: PhantomData
         }
     }
 
-    pub fn get_value(&mut self, val: &[V]) -> R {
-        match self.value {
-            Some(result) => result,
-            None => {
-                self.value = Some((self.calc)(val));
-                self.value.unwrap()
-            }
+    pub fn get_value(&mut self, val: &'a [V]) -> R {
+        if self.value.contains_key(val) {
+            return self.value.get(val).unwrap().clone();
+        } else {
+            self.value.insert(val, (self.calc)(val));
+            return self.value.get(val).unwrap().clone();
         }
     }
 }
@@ -45,19 +43,19 @@ mod tests {
     fn works_with_integers() {
         let mut c = Cacher::new(|val| val[0]);
         assert_eq!(c.get_value(&[1]), 1);
-        assert_eq!(c.get_value(&[5]), 1);
+        assert_eq!(c.get_value(&[5]), 5);
     }
 
     #[test]
     fn works_with_str() {
         let mut c = Cacher::new(|val| val[0]);
         assert_eq!(c.get_value(&["test"]), "test");
-        assert_eq!(c.get_value(&["fsfsf"]), "test");
+        assert_eq!(c.get_value(&["second test"]), "second test");
     }
 
     #[test]
     fn works_with_different_return_types() {
-        let mut c = Cacher::new(|val| "test");
+        let mut c = Cacher::new(|_| "test");
         assert_eq!(c.get_value(&[1]), "test");
     }
 
@@ -65,5 +63,11 @@ mod tests {
     fn works_with_bigger_slice() {
         let mut c = Cacher::new(|val| val[0] + val[1]);
         assert_eq!(c.get_value(&[1, 2]), 3);
+    }
+
+    #[test]
+    fn works_with_string() {
+        let mut c = Cacher::new(|val| String::from(val[0]));
+        assert_eq!(c.get_value(&["test"]), String::from("test"));
     }
 }
